87c80d1a42ba55dd6d057f37787f10569317bbd7
sugar-cat <sugar-cat@sugar-catnoMac-mini.local>
2025-08-05 20:56:45 +0900

    add member

---
+++ service/server/infra/repository/schema/migrations/0013_young_hellcat.sql
@@ -0,0 +1,12 @@
+INSERT INTO channel (
+    id,
+    platform_channel_id,
+    creator_id,
+    platform_type,
+    title,
+    description,
+    published_at,
+    subscriber_count,
+    thumbnail_url
+) VALUES
+    ('e2113e44-de27-4139-878d-7c199b4773a7', '1184405770', '36bc2f5d-89b7-45a0-82ec-64e2902f3287', 'twitch', '紡木こかげ', '', '2020-01-01T00:00:00Z', 0, '');
